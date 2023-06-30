329303f77a1ec945412dfb0ff5b3893c0de5986e
Philip McCrickard <pcricket10@gmail.com>
2023-06-30 16:34:11 -0400

    space-invaders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// use piston_window::*;
+
+mod game;
+// mod drawing;
+// use game::Game;
+
 fn main() {
-    println!("Hello, world!");
+    game::main();
+    // let mut window: PistonWindow = WindowSettings::new("Space Invaders", [WIDTH, HEIGHT])
+    //     .exit_on_esc(true)
+    //     .build()
+    //     .unwrap();
+
+    // let (width, height) = (640.0, 480.0);
+    // // let mut game = Game::new(width, height);
 }
